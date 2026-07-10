b97f7e48c695741e59193022f78ff5c67df11c42
sokheng429-wq <sokheng429@gmail.com>
2026-07-10 20:50:33 +0700

    Fix build errors

--- src/Page/Addproduct.jsx
+++ src/Page/Addproduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import "./AddProduct.css";
+import "./Addproduct.css";
 import {
   getAllProducts,
   getProductById,
